0003f31785fdedd6433c4a332f641c3260b91b51
fedorkotov30 <kotovfedor30@gmail.com>
2023-09-08 17:14:46 +0300

    ver [1.0]

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const appRouter = require('./routes/index');
+
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb ' } = process.env;
+
+const app = express();
+
+mongoose.connect(DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '64fb2a12178dbdd9890f8e5c', // _id созданного пользователя
+  };
+
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(appRouter);
+
+app.listen(PORT);
---
+++ models/user.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Поле "name" должно быть заполнено'],
+      minlength: [2, 'Минимальное количество символов - 2'],
+      maxlength: [30, 'Максимальное количество символов - 30'],
+    },
+    about: {
+      type: String,
+      required: [true, 'Поле "about" должно быть заполнено'],
+      minlength: [2, 'Минимальное количество символов - 2'],
+      maxlength: [30, 'Максимальное количество символов - 30'],
+    },
+    avatar: {
+      type: String,
+      required: [true, 'Поле "avatar" должно быть заполнено'],
+      validate: {
+        validator(url) {
+          const res = url.match(
+            /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/g,
+          );
+          return res !== null;
+        },
+        message: 'Введите URL',
+      },
+    },
+  },
+  { versionKey: false },
+);
+
+module.exports = mongoose.model('user', userSchema);
